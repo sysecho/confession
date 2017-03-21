66ed060a468662a3cac91b1a0a351ee5839c8209
鲜小飞 <18216433389@163.com>
2017-03-21 11:03:29 +0800

    去掉输出语句

--- src/main/java/com/confession/utils/PageUtiles.java
+++ src/main/java/com/confession/utils/PageUtiles.java
@@ -16,7 +16,6 @@ public class PageUtiles {
      */
 	public static List<Object> page(List<Object> list, int page, int size){
 		Collections.sort(list, new ComparatorUtils());
-		System.out.println("第"+page+"页，每页"+size+"条数据。");
 		int len = page+size>list.size()?list.size():page+size;
         List<Object> reList = new ArrayList<Object>();
         for (; page < len; page++) {
