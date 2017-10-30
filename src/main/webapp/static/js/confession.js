fc70ff90975048b047bc6f6c040b714497e15e49
sysecho <707406343@qq.com>
2017-10-30 11:29:01 +0800

    界面调整

--- src/main/webapp/static/js/confession.js
+++ src/main/webapp/static/js/confession.js
@@ -1,12 +1,21 @@
 function submitForm(){
 	var content = $("#content").val();
-	if(content){
-		$.post("submit",{content:content,name:$("#name").val(),fromeUser:$("#fromeUser").val()},function(data){
-			if(data.success == true){
-				$.alert("留言成功啦，等带微信推送吧~~");
-			}
-		});
-	}else{
-		$.alert('难道一句话都没有吗？~');
+	var name = $("#name").val();
+	if(!name){
+		$.toast('亲，你要给谁表白呢(￣▽￣)~*？');
+		return;
 	}
+	if(!content){
+		$.toast('难道一句话都没有吗？~');
+		return;
+	}
+	$.showPreloader('发送中......请稍等。');
+	$.post("submit",{content:content,name:name,fromeUser:$("#fromeUser").val()},function(data){
+		if(data.success == true){
+			$.hidePreloader();
+			 $.alert("亲留言成功啦，我们会帮你推送的哦~~，确定后将关闭窗口", function () {
+				 window.close();
+		     });
+		}
+	});
 }
--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -24,6 +24,7 @@ function addItems(page, size) {
 			 // 重置加载flag
 	        loading = false;
 	        $('.infinite-scroll-preloader').addClass('loadHide');
+	        $('.infinite-scroll-preloader').remove();
 		}
 	});
 }
